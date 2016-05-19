c637382a8eb21a39866e22cb366b5fb3aa632c1e
jonayerdi <jon_ayerdi_33@hotmail.es>
2016-05-20 00:02:43 +0200

    Fixed previous merge

--- src/main/java/data/MessageReader.java
+++ src/main/java/data/MessageReader.java
@@ -2,7 +2,16 @@ package data;
 
 import java.io.IOException;
 
+/**
+ * Generic class that allows the automatic casting of the data Object within MessagePublish or MessagePublication.
+ */
 public class MessageReader <T> {
+	/**
+     * This method reads and returns the casted data Object within a MessagePublish or MessagePublication.
+     *
+     * @param msg The message from which the object must be read.
+     * @return The casted object within the provided MessagePublish or MessagePublication.
+     */
 	@SuppressWarnings("unchecked")
 	public T readObject(MessagePublish msg) throws ClassNotFoundException, IOException {
 		return (T)msg.getDataObject();
