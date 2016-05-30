2c6fb9ff886b63bc0c3029da034e2fa481f9d381
Gorka Olalde Mendia <gorka.olalde@gmail.com>
2016-05-31 01:07:29 +0200

    Added documentation to message unsubscribe class

--- src/main/java/data/MessageUnsubscribe.java
+++ src/main/java/data/MessageUnsubscribe.java
@@ -8,14 +8,23 @@ import java.io.UnsupportedEncodingException;
  * @author urko
  */
 public class MessageUnsubscribe extends MessageSubscribe {
-	public MessageUnsubscribe(String ... topics) {
+    /**
+     * Constructor to build an unsubscribe message based on a topic array.
+     * @param topics The topics to store in the message.
+     */
+    public MessageUnsubscribe(String ... topics) {
         super(topics);
     }
-    
+
+    /**
+     * Constructor to build an unsubscribe message based on it's raw byte array.
+     * @param origin The byte array where the message is stored.
+     * @throws UnsupportedEncodingException
+     */
     public MessageUnsubscribe(byte[] origin) throws UnsupportedEncodingException {
     	super(origin);
     }
-    
+
     public int getMessageType() {
 		return Message.MESSAGE_UNSUBSCRIBE;
 	}
